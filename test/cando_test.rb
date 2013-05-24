3cbdde8e12e3b23a3cf54098a2396a96b38e2390
Krugloff <mr.krugloff@gmail.com>
2013-05-24 23:55:46 +0400

    Refactoring test for module Cando.

--- test/cando_test.rb
+++ test/cando_test.rb
@@ -1,16 +1,18 @@
 require_relative 'test_helper'
 
 class CandoTest < Test::Unit::TestCase
-  def self.startup
-    Cando.authorize(Controller) { |auth| auth.for_owner :create }
-      .authorize(Controller) { |auth| auth.for_client :index }
-  end
-
   def self.shutdown
     Controller.filters.clear
   end
 
+  setup do
+    @before = Controller.filters.size
+  end
+
   test 'authorize' do
-    assert_equal Controller.filters.size, 2
+    Cando .authorize(Controller) { |auth| auth.for_owner :create }
+          .authorize(Controller) { |auth| auth.for_client :index }
+
+    assert @before < Controller.filters.size
   end
 end
